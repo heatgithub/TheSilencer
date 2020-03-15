db35e0fe23a4673718989b96e1d33d1606cc6f59
Henrik Ericson <henrik@hericson.se>
2020-03-15 15:14:32 +0100

    New main form layout. Now detecting music, advertisement and paused play.

--- TheSilencer/MainForm.Designer.cs
+++ TheSilencer/MainForm.Designer.cs
@@ -28,44 +28,116 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
-            this.txtBox = new System.Windows.Forms.TextBox();
-            this.btnRefresh = new System.Windows.Forms.Button();
+            this.txtBoxInfo = new System.Windows.Forms.TextBox();
+            this.labelVersion = new System.Windows.Forms.Label();
+            this.labelMusicProgram = new System.Windows.Forms.Label();
+            this.labelDelay = new System.Windows.Forms.Label();
+            this.textBoxMusicProgram = new System.Windows.Forms.TextBox();
+            this.textBoxDelay = new System.Windows.Forms.TextBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.Stop = new System.Windows.Forms.Button();
+            this.TheTimer = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
-            // txtBox
+            // txtBoxInfo
             // 
-            this.txtBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.txtBoxInfo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtBox.Location = new System.Drawing.Point(12, 41);
-            this.txtBox.Multiline = true;
-            this.txtBox.Name = "txtBox";
-            this.txtBox.ReadOnly = true;
-            this.txtBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtBox.Size = new System.Drawing.Size(776, 397);
-            this.txtBox.TabIndex = 0;
-            this.txtBox.TabStop = false;
+            this.txtBoxInfo.Location = new System.Drawing.Point(12, 64);
+            this.txtBoxInfo.Multiline = true;
+            this.txtBoxInfo.Name = "txtBoxInfo";
+            this.txtBoxInfo.ReadOnly = true;
+            this.txtBoxInfo.Size = new System.Drawing.Size(360, 67);
+            this.txtBoxInfo.TabIndex = 6;
+            this.txtBoxInfo.TabStop = false;
             // 
-            // btnRefresh
+            // labelVersion
             // 
-            this.btnRefresh.Location = new System.Drawing.Point(12, 12);
-            this.btnRefresh.Name = "btnRefresh";
-            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
-            this.btnRefresh.TabIndex = 1;
-            this.btnRefresh.Text = "Refresh";
-            this.btnRefresh.UseVisualStyleBackColor = true;
-            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            this.labelVersion.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelVersion.Location = new System.Drawing.Point(12, 134);
+            this.labelVersion.Name = "labelVersion";
+            this.labelVersion.Size = new System.Drawing.Size(360, 18);
+            this.labelVersion.TabIndex = 7;
+            this.labelVersion.Text = "label1";
+            this.labelVersion.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // labelMusicProgram
+            // 
+            this.labelMusicProgram.AutoSize = true;
+            this.labelMusicProgram.Location = new System.Drawing.Point(12, 15);
+            this.labelMusicProgram.Name = "labelMusicProgram";
+            this.labelMusicProgram.Size = new System.Drawing.Size(79, 13);
+            this.labelMusicProgram.TabIndex = 0;
+            this.labelMusicProgram.Text = "Music program:";
+            // 
+            // labelDelay
+            // 
+            this.labelDelay.AutoSize = true;
+            this.labelDelay.Location = new System.Drawing.Point(12, 41);
+            this.labelDelay.Name = "labelDelay";
+            this.labelDelay.Size = new System.Drawing.Size(59, 13);
+            this.labelDelay.TabIndex = 2;
+            this.labelDelay.Text = "Delay (ms):";
+            // 
+            // textBoxMusicProgram
+            // 
+            this.textBoxMusicProgram.Location = new System.Drawing.Point(97, 12);
+            this.textBoxMusicProgram.Name = "textBoxMusicProgram";
+            this.textBoxMusicProgram.Size = new System.Drawing.Size(150, 20);
+            this.textBoxMusicProgram.TabIndex = 1;
+            // 
+            // textBoxDelay
+            // 
+            this.textBoxDelay.Location = new System.Drawing.Point(97, 38);
+            this.textBoxDelay.Name = "textBoxDelay";
+            this.textBoxDelay.Size = new System.Drawing.Size(75, 20);
+            this.textBoxDelay.TabIndex = 3;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonStart.Location = new System.Drawing.Point(297, 10);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 4;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // Stop
+            // 
+            this.Stop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Stop.Location = new System.Drawing.Point(297, 36);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(75, 23);
+            this.Stop.TabIndex = 5;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // TheTimer
+            // 
+            this.TheTimer.Tick += new System.EventHandler(this.TheTimer_Tick);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btnRefresh);
-            this.Controls.Add(this.txtBox);
+            this.ClientSize = new System.Drawing.Size(384, 161);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.textBoxDelay);
+            this.Controls.Add(this.textBoxMusicProgram);
+            this.Controls.Add(this.labelDelay);
+            this.Controls.Add(this.labelMusicProgram);
+            this.Controls.Add(this.labelVersion);
+            this.Controls.Add(this.txtBoxInfo);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MinimumSize = new System.Drawing.Size(600, 400);
+            this.MinimumSize = new System.Drawing.Size(400, 200);
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
@@ -77,8 +149,15 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox txtBox;
-        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.TextBox txtBoxInfo;
+        private System.Windows.Forms.Label labelVersion;
+        private System.Windows.Forms.Label labelMusicProgram;
+        private System.Windows.Forms.Label labelDelay;
+        private System.Windows.Forms.TextBox textBoxMusicProgram;
+        private System.Windows.Forms.TextBox textBoxDelay;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.Timer TheTimer;
     }
 }
 
--- TheSilencer/MainForm.cs
+++ TheSilencer/MainForm.cs
@@ -6,46 +6,193 @@ namespace TheSilencer
 {
     public partial class MainForm : Form
     {
+        enum MusicProgramActivity
+        {
+            NotPlaying,
+            Playing,
+            Advertisement,
+            Unknown
+        }
+
+
+        /// <summary>
+        /// The Main Form
+        /// </summary>
         public MainForm()
         {
             InitializeComponent();
         }
 
+        
+        /// <summary>
+        /// While the main form loads
+        /// </summary>
         private void MainForm_Load(object sender, EventArgs e)
         {
-            // display the name and version as the forms title
-            this.Text = "The Silencer - v" + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
-            this.Text += " (" + System.IO.File.GetLastWriteTime(System.Reflection.Assembly.GetExecutingAssembly().Location).ToString("yyyy-MM-dd") + ")";
+            // set the name as the forms title
+            this.Text = "The Silencer";
+
+            // display the name and version
+            labelVersion.Text = "The Silencer - v" + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            labelVersion.Text += " (" + System.IO.File.GetLastWriteTime(System.Reflection.Assembly.GetExecutingAssembly().Location).ToString("yyyy-MM-dd") + ")";
+
+            // enter default values
+            textBoxMusicProgram.Text = "Visual Studio".Substring(7, 1) + '\u0070'.ToString() + 'o'.ToString() + ((char)(232 / 2)).ToString() + "i" + ((char)102).ToString() + ((char)('$' + 'U')).ToString();
+            textBoxDelay.Text = "500";
 
             // force a refresh
-            btnRefresh_Click(sender, e);
+            //btnRefresh_Click(sender, e);
+        }
+        
+
+        /// <summary>
+        /// Clicking the Start button
+        /// </summary>
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            string musicProgramName = textBoxMusicProgram.Text.Trim();
+            string musicProgramWindowTitle = GetWindowTitleFromProcessByName(musicProgramName);
+            int musicProgramId = GetIdFromProcessByName(musicProgramName);
+
+            // show info about the music program
+            DisplayMusicProgramInfo(musicProgramId, musicProgramName);
+
+            // start the timer
+            textBoxMusicProgram.Enabled = false;
+            textBoxDelay.Enabled = false;
+            TheTimer.Interval = ToInteger(textBoxDelay.Text.Trim());
+            TheTimer.Start();
         }
 
-        private void btnRefresh_Click(object sender, EventArgs e)
-        {
-            txtBox.Text = "";
 
-            WriteAllProcesses();
+        /// <summary>
+        /// Clicking the Stop button
+        /// </summary>
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            // stop the timer
+            textBoxMusicProgram.Enabled = true;
+            textBoxDelay.Enabled = true;
+            TheTimer.Stop();
+        }
+
+
+        /// <summary>
+        /// Display some info in the txtBox about the current music program entered
+        /// </summary>
+        private void DisplayMusicProgramInfo(int musPrgId, string musPrgName)
+        {
+            txtBoxInfo.Text = "";
+
+            // if music program is found
+            if (musPrgId != -1)
+            {
+                txtBoxInfo.Text = "Music program: " + musPrgName;
+                txtBoxInfo.Text += Environment.NewLine;
+                txtBoxInfo.Text += "Process id: " + musPrgId.ToString();
+                txtBoxInfo.Text += Environment.NewLine;
+            }
+            else
+            {
+                txtBoxInfo.Text = "Music program not found!";
+            }
 
             // unselect everything in the textBox
-            txtBox.Select(0, 0);
+            txtBoxInfo.Select(0, 0);
         }
 
-        private void WriteAllProcesses()
-        {
-            Process[] processlist = Process.GetProcesses();
 
-            string txt = "";
-            foreach (Process process in processlist)
+        /// <summary>
+        /// The timer checks for advertisment from the music program
+        /// </summary>
+        private void TheTimer_Tick(object sender, EventArgs e)
+        {
+            string musicProgramName = textBoxMusicProgram.Text.Trim();
+            string musicProgramWindowTitle = GetWindowTitleFromProcessByName(musicProgramName);
+            int musicProgramId = GetIdFromProcessByName(musicProgramName);
+            MusicProgramActivity activity = WhatIsMusicProgramDoing(musicProgramWindowTitle, musicProgramName);
+
+            DisplayMusicProgramInfo(musicProgramId, musicProgramName);
+            if (activity == MusicProgramActivity.NotPlaying) { txtBoxInfo.Text += "Music program is paused"; }
+            if (activity == MusicProgramActivity.Playing) { txtBoxInfo.Text += "Now playing: " + musicProgramWindowTitle; }
+            if (activity == MusicProgramActivity.Advertisement) { txtBoxInfo.Text += "Advertisement is playing"; }
+            if (activity == MusicProgramActivity.Unknown) { txtBoxInfo.Text += "Unknown activity!"; }
+        }
+
+
+        /// <summary>
+        /// Gets the window title for the specified process name
+        /// </summary>
+        /// <param name="processName">Name of the process as string</param>
+        /// <returns>Window title as string</returns>
+        private string GetWindowTitleFromProcessByName(string processName)
+        {
+            // search for the process by name
+            foreach (Process process in Process.GetProcessesByName(processName))
             {
-                if (!String.IsNullOrEmpty(process.MainWindowTitle))
+                if (process.MainWindowTitle.Length > 0)
                 {
-                    txt += "Process: " + process.ProcessName.ToString() + " (ID: " + process.Id.ToString() + ") Window title: " + process.MainWindowTitle + Environment.NewLine;
+                    return process.MainWindowTitle;
                 }
             }
-            txtBox.Text += txt;
 
+            return "";
         }
+
+
+        /// <summary>
+        /// Gets the process id from the specified process name
+        /// </summary>
+        /// <param name="processName">Name of the process as string</param>
+        /// <returns>Process id as integer</returns>
+        private int GetIdFromProcessByName(string processName)
+        {
+            // search for the process by name
+            foreach (Process process in Process.GetProcessesByName(processName))
+            {
+                if (process.MainWindowTitle.Length > 0)
+                {
+                    return process.Id;
+                }
+            }
+
+            return -1;
+        }
+
+
+        /// <summary>
+        /// Guesses what the music program is doing based on the window title
+        /// </summary>
+        /// <param name="windowTitle">Music program window title</param>
+        /// <returns>MusicProgramActivity enum: NonPlaying, Playing, Advertisement, Unknown</returns>
+        private MusicProgramActivity WhatIsMusicProgramDoing(string windowTitle, string musicProgramName)
+        {
+            if (windowTitle.ToLower().Contains(musicProgramName.ToLower())) { return MusicProgramActivity.NotPlaying; }
+            if (windowTitle.ToLower().Contains(" - ")) { return MusicProgramActivity.Playing; }
+            if (windowTitle.ToLower().Contains("advertisement")) { return MusicProgramActivity.Advertisement; }
+            
+            return MusicProgramActivity.Unknown;
+        }
+
+
+        /// <summary>
+        /// Converts the string to an integer
+        /// </summary>
+        /// <param name="value">The string to convert</param>
+        /// <returns>The integer value if success, otherwise zero</returns>
+        private int ToInteger(string value)
+        {
+            try
+            {
+                return int.Parse(value, System.Globalization.CultureInfo.InvariantCulture);
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+
+
 
     }
 }
